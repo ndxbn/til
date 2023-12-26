d2594d8328ec6ec095a1618725a75c4d91e84a93
Sitri Kamishirasawa <sitri.ndxbn@gmail.com>
2023-12-26 14:12:10 +0000

    hozon

--- workspace/2023-12-26.py
+++ workspace/2023-12-26.py
@@ -1,4 +1,3 @@
-#おためしrange
 select_1st = 'エイト'
 select_2nd = 'あーすと'
 select_3rd = 'しとりー'
@@ -10,12 +9,10 @@ top_3rd = 'しとりー'
 x = [select_1st,select_2nd,select_3rd,'スイカselect','泥試合select','エキシビジョンselect'] #順位予想
 y = [top_1st,top_2nd,top_3rd,'スイカtop','泥試合top','エキシビジョンtop'] #順位結果
 
-def compare(n):
-    if (x[n] == y[n]):
-        a = 1
-    else:
-        a = 0
-    print(a)
+current_score = 0
 
 for n in range(3):
-    compare(n)
+    if (x[n] == y[n]):
+        current_score += 1
+
+    print(current_score)
