478cb3f7644fa0a8ee7b00b41b23ea608c7fccb5
Sitri Kamishirasawa <sitri.ndxbn@gmail.com>
2023-12-26 15:01:02 +0000

    とりあえずこれで

--- workspace/2023-12-26.py
+++ workspace/2023-12-26.py
@@ -1,6 +1,3 @@
-selected = ['エイト', 'あーすと', 'しとりー']
-result = ['エイト', 'ぴぴぷ', 'しとりー']
-
 def get_score(result, selected):
     score = 0
     if(result == selected):
@@ -11,7 +8,6 @@ def get_score(result, selected):
     return score
 
 selectedList = []
-resultList = []
 counter = 1
 while True:
     select = input(str(counter) + "にんめ: ")
@@ -20,4 +16,16 @@ while True:
     selectedList.append(select)
     counter += 1
 
-print(selectedList)
+resultList = []
+counter = 1
+while True:
+    result = input(str(counter) + "にんめ: ")
+    if(result == ""): break
+
+    resultList.append(result)
+    counter += 1
+
+my_score = 0
+for n in range(len(resultList)):
+    my_score += get_score(resultList[n], selectedList[n])
+    print(my_score)
